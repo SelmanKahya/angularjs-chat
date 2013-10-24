d5696b2f8fb61e6d91b80c6cf4455d22de8da313
Selman K. <selmanhalid@hotmail.com>
2013-10-24 12:59:07 -0700

    creating /chat/* routes

---
+++ server/routes/chat.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET users listing.
+ */
+
+exports.init = function(req, res){
+    res.send("respond with a resource");
+};
+
+exports.users = function(req, res){
+    res.send("respond with a resource");
+};
