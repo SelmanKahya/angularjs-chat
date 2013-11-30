3019f35654759a615096105796cd756072ef168c
Selman K. <selmanhalid@hotmail.com>
2013-11-30 13:16:04 -0800

    updated server/app.js
    deleted unnecessary app.use expressions

--- server/app.js
+++ server/app.js
@@ -16,13 +16,9 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
