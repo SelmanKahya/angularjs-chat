4f4fe3b6970c8f76c4cd5d8fb1d78ebf5eb4bc82
Selman K. <selmanhalid@hotmail.com>
2013-12-05 13:33:36 -0800

    refactored server/app.js
    - moved environment variables to functions/environment.js
    - moved route definition expressions to functions/route.js
    - commented the code

--- server/app.js
+++ server/app.js
@@ -1,46 +1,30 @@
-/**
- * Module dependencies.
- */
-
 // libs
-var express = require('express');
-var http = require('http');
-var path = require('path');
+express = require('express');
+http = require('http');
+path = require('path');
 
 // functions
 var socket = require('./functions/socket.js');
-
-// routes
-var routes = require('./routes');
-var chat = require('./routes/chat');
+var route = require('./functions/route.js');
+var environment = require('./functions/environment.js');
 
 // application
 app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.use(express.logger('dev'));
-app.use(express.methodOverride());
-app.use(app.router);
+// set all environment variables
+environment.set();
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+// initialize the routes
+route.init();
 
-// route definitions
-app.get('/', routes.index);
-app.set('CHAT_ROUTE', '/chat');
-app.get('/chat/users', chat.users);
-
-// Server is starting, hold on!
+// server is going up, hold on!
 var server = http.createServer(app);
 
 server.listen(app.get('port'), function(){
 
     console.log('Express server listening on port ' + app.get('port'));
 
-    // start listening /chat
+    // install sockjs handlers - /chat
     socket.init(server);
 
 });
