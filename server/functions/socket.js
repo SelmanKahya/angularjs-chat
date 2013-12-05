cf99ff9e1dfa5cbf2070aee50cd465180e25f2bf
Selman K. <selmanhalid@hotmail.com>
2013-12-05 13:48:20 -0800

    new socket function - broadcast
    broadcast given message to all listeners

--- server/functions/socket.js
+++ server/functions/socket.js
@@ -21,43 +21,25 @@ var newConnection = function(conn) {
     }
 
     conn.on('data', function(message) {
-
         var message = JSON.parse(message);
-
-        conn.user = message.user;
-
-        var username = message.user.user_first_name + " " + message.user.user_last_name;
-
-        if(message.type == 'message')  {
-
-            if(message.action == 'new-user'){
-                // tell users that new user entered to room
-                for (var ii=0; ii < connections.length; ii++) {
-                    connections[ii].write(username + " entered to the room");
-                }
-            }
-
-            else if(message.action == 'send'){
-                for (var ii=0; ii < connections.length; ii++) {
-                    connections[ii].write(username + " says: " + message.data);
-                }
-            }
-        }
+        broadcast(conn.user_info.name + ": " + message.data);
     });
 
     // on close, tell all the users that somebody has left the chat room
     conn.on('close', function(){
         var name = conn.user_info.name;
-        for (var ii=0; ii < users.length; ii++) {
-            users.write(name + " has been disconnected!");
-        }
+        broadcast(name + " has been disconnected!");
     });
 
     // tell all the users, somebody has joined
-    for (var ii=0; ii < users.length; ii++) {
-        users.write(conn.user_info.name + " has joined!");
-    }
+    broadcast(conn.user_info.name + " has joined!");
 
     // push new connection into the array
     users.push(conn);
+}
+
+var broadcast = function(message){
+    for (var ii=0; ii < users.length; ii++) {
+        users[ii].write(message);
+    }
 }
