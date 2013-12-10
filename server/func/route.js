37d45fa06d54567d20fd3cc5ffbe3233c950604f
Selman K. <selmanhalid@hotmail.com>
2013-12-10 14:03:47 -0800

    updated comments

--- server/func/route.js
+++ server/func/route.js
@@ -7,7 +7,12 @@ exports.init = function(){
 
     // route definitions
     app.get('/', routes.index);
+
+    // CHAT ROUTES
+    // sockjs will install its handlers on this route:
     app.set('CHAT_ROUTE', '/chat');
+
+    // returns list of online users
     app.get('/chat/users', chat.users);
 
 };
