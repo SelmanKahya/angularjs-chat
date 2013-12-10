1b777bae3eb910c95c3620e75f7c50996471bcf9
Selman K. <selmanhalid@hotmail.com>
2013-12-10 13:56:52 -0800

    updated comment

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ server.listen(app.get('port'), function(){
 
     console.log('Express server listening on port ' + app.get('port'));
 
-    // install sockjs handlers - /chat
+    // install sockjs handlers
     socket.init(server);
 
 });
