cb40d56714173ca8c1dcd3ae86c299d47cdfb5be
Selman K. <selmanhalid@hotmail.com>
2013-12-23 18:29:10 -0800

    implementing start function - chat room

--- angular-app/app/scripts/controllers/main.js
+++ angular-app/app/scripts/controllers/main.js
@@ -1,10 +1,17 @@
 'use strict';
 
-angular.module('angularAppApp')
-    .controller('MainCtrl', function ($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
-    });
+angular.module('angularAppApp').controller('MainCtrl', function ($scope) {
+
+    $scope.chat = {
+        username: '',
+        started: false
+    }
+
+    // checks the username and init chat room
+    $scope.start = function(){
+        if($scope.username){
+            $scope.chat.username = $scope.username;
+            $scope.chat.started = true;
+        }
+    }
+});
