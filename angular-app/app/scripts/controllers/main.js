0195c8943e271f41af818d60d29e3a966b070a3e
Selman K. <selmanhalid@hotmail.com>
2013-12-23 18:36:38 -0800

    implemented sendMessage func - chat room

--- angular-app/app/scripts/controllers/main.js
+++ angular-app/app/scripts/controllers/main.js
@@ -2,6 +2,11 @@
 
 angular.module('angularAppApp').controller('MainCtrl', function ($scope) {
 
+    var sock = new SockJS('http://localhost:3000/chat');
+
+    $scope.messageText = "";
+    $scope.history = "";
+
     $scope.chat = {
         username: '',
         started: false
@@ -14,4 +19,11 @@ angular.module('angularAppApp').controller('MainCtrl', function ($scope) {
             $scope.chat.started = true;
         }
     }
+
+    // sends the message
+    $scope.sendMessage = function() {
+        sock.send(JSON.stringify({type: 'message', data: $scope.messageText}));
+        $scope.history += '\n' + $scope.history;
+        $scope.messageText = "";
+    };
 });
