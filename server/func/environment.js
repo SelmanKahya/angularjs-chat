60539e0d5187e49d72319fd00a5522ec4801bd1a
Selman K. <selmanhalid@hotmail.com>
2014-01-06 22:26:32 -0800

    server - allow CORS

--- server/func/environment.js
+++ server/func/environment.js
@@ -12,4 +12,11 @@ exports.set = function(){
         app.use(express.errorHandler());
     }
 
+    // allow cross site scripting, be careful with that in production
+    app.all('*', function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    });
 };
