e30844484b945d00bcf91dd1fdc8a808d2dabe45
Selman K. <selmanhalid@hotmail.com>
2014-01-07 22:37:39 -0800

    updating api service

--- angular-app/app/scripts/services/api.js
+++ angular-app/app/scripts/services/api.js
@@ -1,11 +1,19 @@
 'use strict';
 
-angular.module('angularAppApp').factory('userService', function($http) {
+angular.module('angularAppApp').factory('chatService', function($http) {
     return {
-        online : function(callback){
+
+        // returns sockjs object
+        socket : function(){
+            return new SockJS('http://localhost:3000/chat', null, {debug: true});
+        },
+
+        // returns the list of online users
+        onlineUsers : function(callback){
             $http({method: 'GET', url: 'http://localhost:3000/users'}).success(function(response, status, headers, config) {
                 callback(response);
             });
         }
+
     }
 });
