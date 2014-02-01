e401ca428cc1e210f59f6e8f7e35c16fa67b8967
Selman K. <selmanhalid@hotmail.com>
2014-01-31 19:00:30 -0800

    adding comments

--- angular-app/app/scripts/app.js
+++ angular-app/app/scripts/app.js
@@ -2,11 +2,15 @@
 
 angular.module('angularAppApp', ['ngCookies', 'ngResource', 'ngSanitize', 'ngRoute']).config(function ($routeProvider) {
     $routeProvider
+
+        // One and the only route: /
         .when('/', {
             templateUrl: 'views/main.html',
             controller: 'MainCtrl'
         })
+
         .otherwise({
             redirectTo: '/'
         });
+
 });
--- angular-app/app/scripts/services/api.js
+++ angular-app/app/scripts/services/api.js
@@ -3,16 +3,9 @@
 angular.module('angularAppApp').factory('chatService', function($http) {
     return {
 
-        // returns sockjs object
+        // RETURNS SockJS object
         socket : function(){
             return new SockJS('http://localhost:3000/chat', null, {debug: true});
-        },
-
-        // returns the list of online users
-        onlineUsers : function(callback){
-            $http({method: 'GET', url: 'http://localhost:3000/users'}).success(function(response, status, headers, config) {
-                callback(response);
-            });
         }
 
     }
