ca1af339ead38d3c3e6a2506cfae5db888c9a262
Selman K. <selmanhalid@hotmail.com>
2014-01-31 19:01:37 -0800

    updated main controller

--- angular-app/app/scripts/controllers/main.js
+++ angular-app/app/scripts/controllers/main.js
@@ -2,6 +2,25 @@
 
 angular.module('angularAppApp').controller('MainCtrl', function ($scope, $route, $timeout, chatService) {
 
+    // TODO:
+    // Create a class: request
+    // Use it in send(type, data) function
+
+    // TODO:
+    // Create a class: response
+    // Use it in onMessage(message) function
+
+    // REQUEST TYPE
+    // This needs to defined in the server as well
+    // All requests and responses should carry request type information
+    var REQUEST_TYPE = {
+        NEW_MESSAGE: 1,
+        USER_JOINED: 2,
+        USER_DISCONNECTED: 3,
+        USER_LIST : 4
+    }
+
+    // Chat object - holds all the information
     $scope.chat = {
 
         socket: null,
@@ -19,41 +38,90 @@ angular.module('angularAppApp').controller('MainCtrl', function ($scope, $route,
         }
     }
 
-    // checks the username and init chat room
+    /////////////////////////////////////////////////
+    // HELPER FUNCTION - init
+    // Checks the username and create the socket object
     $scope.init = function(){
+
         if($scope.username){
+
+            // create SockJS object
             var socket = chatService.socket();
+
+            // socket events
             socket.onopen = $scope.onOpen;
             socket.onmessage = $scope.onMessage;
             socket.onerror = $scope.onError;
             socket.onclose = $scope.onError;
+
+            // assigning socket object
             $scope.chat.socket = socket;
+
         }
+
     }
-    $scope.quit = function() {
-        $scope.chat.socket.close();
-        $route.reload();
+
+    /////////////////////////////////////////////////
+    // HELPER FUNCTION - send
+    // Sends a message to the server
+    $scope.send = function(type, data) {
+        $scope.chat.socket.send(JSON.stringify({type: type, data: data}));
     };
 
-    // successfully connected to the server
+    /////////////////////////////////////////////////
+    // HELPER FUNCTION - scrollBottom
+    // After a new line added to chat history text area, scroll to the bottom to see it.
+    // Otherwise, new messages in the chat window won't be shown to the user.
+    $scope.scrollBottom = function() {
+        angular.element("#chat-history").animate({ scrollTop: angular.element("#chat-history")[0].scrollHeight - angular.element("#chat-history").height() }, 500)
+    };
+
+    /////////////////////////////////////////////////
+    // SOCKJS EVENT: ON.OPEN
+    // Client successfully connected to the server
     $scope.onOpen = function(e) {
-        $scope.chat.data.username = $scope.username;
+
+        // socket created successfully
         $scope.chat.status.started = true;
         $scope.chat.status.error = null;
-        $scope.chat.socket.send(JSON.stringify({type: 'introduce', data: $scope.chat.data.username}));
-        $scope.checkOnlineUsers();
+
+        // let the server know new user's user name
+        $scope.chat.data.username = $scope.username;
+        $scope.send(REQUEST_TYPE.USER_JOINED, $scope.chat.data.username);
+
+        // call apply to keep the UI updated
         $scope.$apply();
+
     };
 
-    // message came from the server
-    $scope.onMessage = function(e) {
-        $scope.chat.data.history += e.data + '\n';
-        $scope.scrollBottom();
-        $scope.$apply();
+    /////////////////////////////////////////////////
+    // SOCKJS EVENT: ON.MESSAGE
+    // There is a message coming from the server, handle carefully
+    $scope.onMessage = function(message) {
+
+        var response = angular.fromJson(message.data);
+
+        if(response.type == REQUEST_TYPE.NEW_MESSAGE || response.type == REQUEST_TYPE.USER_JOINED || response.type == REQUEST_TYPE.USER_DISCONNECTED){
+            // incoming chat message
+            $scope.chat.data.history += response.data + '\n';
+            $scope.scrollBottom();
+            $scope.$apply();
+        }
+
+        else if(response.type == REQUEST_TYPE.USER_LIST){
+            // retrieving list of the users from the server
+            var users = angular.fromJson(response.data);
+            $scope.chat.data.users = users;
+            $scope.$apply();
+        }
+
     };
 
-    // sends the message
+    /////////////////////////////////////////////////
+    // SOCKJS EVENT: ON.ERROR
+    // IF there is an error, hide chat console, show the error message to the user
     $scope.onError = function(e) {
+
         // stop chat, show error message to the user
         $scope.chat.status.error = e.reason;
         $scope.chat.status.started = false;
@@ -61,27 +129,27 @@ angular.module('angularAppApp').controller('MainCtrl', function ($scope, $route,
         // print error
         console.log('onerror: ', e);
         $scope.$apply();
+
     };
 
-    // sends the message
-    $scope.sendMessage = function() {
-        $scope.chat.socket.send(JSON.stringify({type: 'message', data: $scope.chat.data.messageText}));
+    /////////////////////////////////////////////////
+    // EVENT: SEND BUTTON CLICK
+    // Client sending a message to the server
+    $scope.sendButtonClicked = function() {
+
+        // send a message in a proper format
+        $scope.send(REQUEST_TYPE.NEW_MESSAGE, $scope.chat.data.messageText);
+
+        // clear message box in the UI
         $scope.chat.data.messageText = '';
-        $scope.scrollBottom();
+
     };
 
-    // periodically checks online users, retrieve list of the users from the server
-    $scope.checkOnlineUsers = function() {
-        chatService.onlineUsers(function(result){
-            $scope.chat.data.users = result;
-            $timeout($scope.checkOnlineUsers, 5000);
-        });
-    }
-
-    // scroll to the bottom, after new line concat
-    $scope.scrollBottom = function(){
-        angular.element("#chat-history").animate({
-            scrollTop: angular.element("#chat-history")[0].scrollHeight - angular.element("#chat-history").height()
-        }, 500)
-    }
+    /////////////////////////////////////////////////
+    // EVENT: QUIT BUTTON CLICK
+    // Close socket, reload the page, so that user can start chatting again
+    $scope.quit = function() {
+        $scope.chat.socket.close();
+        $route.reload();
+    };
 });
