eff55807087d07a4ec939bb3f2bfed51e91cbc32
Selman K. <selmanhalid@hotmail.com>
2014-02-02 20:21:02 -0800

    build

---
+++ scripts/services/api.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// var server = "http://localhost:3000/chat";
+var server = "http://angularjs-chat-server.aws.af.cm/chat";
+
+angular.module('angularAppApp').factory('chatService', function($http) {
+    return {
+
+        // RETURNS SockJS object
+        socket : function(){
+            return new SockJS(server, null, {debug: true});
+        }
+
+    }
+});
