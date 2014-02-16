190326ad19b8ef26887550d9055fe0f5be1e82bd
Selman K <selmanhalid@hotmail.com>
2014-02-15 16:08:21 -0800

    updating config --OpenShift
    no longer using appfog for the demo website

--- node-server/server.js
+++ node-server/server.js
@@ -19,11 +19,16 @@ route.init();
 // server is going up, hold tight!
 var server = http.createServer(app);
 
+// No longer using appfog
 // Decides which port to use
 // AppFog port env. variable: process.env.VCAP_APP_PORT
-var port = parseInt(process.env.VCAP_APP_PORT) || app.get('port');
+// var port = parseInt(process.env.VCAP_APP_PORT) || app.get('port');
 
-server.listen(port, function(){
+// Use OpenShift default port
+var port    = process.env.OPENSHIFT_NODEJS_PORT || app.get('port');
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+
+server.listen(port, ipaddress, function(){
 
     console.log('Express server listening on port ' + port);
 
